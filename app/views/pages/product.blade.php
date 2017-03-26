1137265b05328b4dfca0b0abc00fe5c08461fa08
Jeroen Vijgen <jjvijgen@gmail.com>
2017-03-26 04:09:54 +0200

    Missed a button...

--- app/views/pages/product.blade.php
+++ app/views/pages/product.blade.php
@@ -7,6 +7,8 @@
         <div class="nDescription">Description: {{ $product->large_desc }}</div>
         <div class="nPrice"><span class="glyphicon glyphicon-euro"></span> {{ $product->price }}</div>
         <a href="{{ URL::previous() }}" class="btn btn-raised">Back</a>
-        <a href="{{ url('/cart/add/'.$product->id ) }}" class="btn btn-raised">Buy!</a>
+        @if($product->piece != 0)
+            <a href="{{ url('/cart/add/'. $product->id ) }}" class="btn btn-raised btn-primary" >Buy!</a>
+        @endif
     </div>
 @stop
