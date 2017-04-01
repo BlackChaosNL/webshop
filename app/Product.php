f63b8fb13c5b8a567e34fc57663b5fdde428551e
Jeroen Vijgen <jjvijgen@gmail.com>
2017-04-01 15:35:45 +0200

    WTF did I do here

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $table = "Product";
     // Fillable items
     protected $fillable = array('category', 'small_desc', 'large_desc', 'picture', 'price');
 
