b0f900bd4db4d45a9974b2402c2ee304c58411c1
Jeroen Vijgen <jjvijgen@gmail.com>
2017-04-01 15:37:44 +0200

    Why is this products?

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $table = "product";
     // Fillable items
     protected $fillable = array('category', 'small_desc', 'large_desc', 'picture', 'price');
 
